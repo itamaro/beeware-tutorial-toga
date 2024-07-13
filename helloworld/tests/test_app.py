a562b5ad37977327c26c9d1728c169b615f636ed
Itamar Oren <itamarost@gmail.com>
2024-07-13 15:56:03 -0700

    test greeting
    ```
    $ briefcase dev --test -r                                                                                                                  [15:54:23]
    
    [helloworld] Installing requirements...
    Requirement already satisfied: toga-cocoa~=0.4.5 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (0.4.5)
    Requirement already satisfied: std-nslog~=1.0.0 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (1.0.3)
    Requirement already satisfied: pytest in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (8.2.2)
    Requirement already satisfied: fonttools<5.0.0,>=4.42.1 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from toga-cocoa~=0.4.5) (4.53.1)
    Requirement already satisfied: rubicon-objc<0.5.0,>=0.4.7 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from toga-cocoa~=0.4.5) (0.4.9)
    Requirement already satisfied: toga-core==0.4.5 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from toga-cocoa~=0.4.5) (0.4.5)
    Requirement already satisfied: travertino>=0.3.0 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from toga-core==0.4.5->toga-cocoa~=0.4.5) (0.3.0)
    Requirement already satisfied: iniconfig in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from pytest) (2.0.0)
    Requirement already satisfied: packaging in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from pytest) (24.1)
    Requirement already satisfied: pluggy<2.0,>=1.5 in /Users/itamaro/work/beeware-tutorial/beewarenv/lib/python3.12/site-packages (from pytest) (1.5.0)
    Installing dev requirements... done
    
    [helloworld] Running test suite in dev environment...
    ===========================================================================
    ============================= test session starts ==============================
    platform darwin -- Python 3.12.4, pytest-8.2.2, pluggy-1.5.0 -- /Users/itamaro/work/beeware-tutorial/beewarenv/bin/python
    cachedir: /var/folders/8f/h1vmzw5s5jv8wl9_msv5xq0c0000gn/T/.pytest_cache
    rootdir: /Users/itamaro/work/beeware-tutorial/helloworld
    configfile: pyproject.toml
    collecting ... collected 3 items
    
    tests/test_app.py::test_name PASSED                                      [ 33%]
    tests/test_app.py::test_empty PASSED                                     [ 66%]
    tests/test_app.py::test_brutus FAILED                                    [100%]
    
    =================================== FAILURES ===================================
    _________________________________ test_brutus __________________________________
    
        def test_brutus():
    >       assert greeting("Brutus") == "BeeWare the IDEs of Python!"
    E       AssertionError: assert 'Hello, Brutus' == 'BeeWare the IDEs of Python!'
    E
    E         - BeeWare the IDEs of Python!
    E         + Hello, Brutus
    
    tests/test_app.py:13: AssertionError
    =========================== short test summary info ============================
    FAILED tests/test_app.py::test_brutus - AssertionError: assert 'Hello, Brutus' == 'BeeWare the IDEs of Python!'
    
      - BeeWare the IDEs of Python!
      + Hello, Brutus
    ========================= 1 failed, 2 passed in 0.04s ==========================
    
    [helloworld] Test suite failed!
    (beewarenv) FAIL
    ```

--- helloworld/tests/test_app.py
+++ helloworld/tests/test_app.py
@@ -1,3 +1,13 @@
-def test_first():
-    """An initial test for the app."""
-    assert 1 + 1 == 2
+from helloworld.app import greeting
+
+
+def test_name():
+    assert greeting("Alice") == "Hello, Alice"
+
+
+def test_empty():
+    assert greeting("") == "Hello, stranger"
+
+
+def test_brutus():
+    assert greeting("Brutus") == "BeeWare the IDEs of Python!"

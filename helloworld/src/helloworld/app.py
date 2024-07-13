f62208fc457a83b3435d0e2a0f22d6aaf3959447
Itamar Oren <itamarost@gmail.com>
2024-07-13 15:47:05 -0700

    Tutorial 3: Use the camera

--- helloworld/src/helloworld/app.py
+++ helloworld/src/helloworld/app.py
@@ -31,6 +31,16 @@ class HelloWorld(toga.App):
         main_box.add(name_box)
         main_box.add(button)
 
+        self.photo = toga.ImageView(style=Pack(height=300, padding=5))
+        camera_button = toga.Button(
+            "Take photo", 
+            on_press=self.take_photo, 
+            style=Pack(padding=5)
+        )
+
+        main_box.add(self.photo)
+        main_box.add(camera_button)
+
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
@@ -41,6 +51,25 @@ class HelloWorld(toga.App):
             f"Hello, {self.name_input.value or 'stranger'}",
             "Hi there!"
         )
+
+    async def take_photo(self, widget, **kwargs):
+        try:
+            if not self.camera.has_permission:
+                await self.camera.request_permission()
+
+            image = await self.camera.take_photo()
+            if image:
+                self.photo.image = image
+        except NotImplementedError:
+            await self.main_window.info_dialog(
+                "Oh no!",
+                "The Camera API is not implemented on this platform",
+            )
+        except PermissionError:
+            await self.main_window.info_dialog(
+                "Oh no!",
+                "You have not granted permission to take photos",
+            )
 
 
 def main():
